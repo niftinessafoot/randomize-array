d0c9f30cd00238f7b23f20f8a14770ef17fc8092
Matthew Smith <niftinessafoot@gmail.com>
2022-12-15 14:21:14 -0500

    ci: integrate multiple packages based on use

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
-type ArrayProp = any[];
-type ArrayOutput = any[];
+/**
+ * A library for returning a randomly-indexed array.
+ *
+ * @remarks
+ * Takes in an array and returns a new {@link ArrayOutput | array} with those indexes randomly dispersed. The array contents can be anything, though you’re likely to get the most value out of a number or string.
+ *
+ * @packageDocumentation
+ */
+
+export type ArrayProp = unknown[];
+export type ArrayOutput = unknown[];
 
 /**
+ * Returns a new array, randomly re-indexed.
  *
- * @param arr Array to randomize.
- * @returns Ranomied array.
+ * @param arr - {@link ArrayProp | Array} to randomize
+ * @returns New copy of randomly-sorted {@link ArrayOutput | array}
  */
 function RandomizeArray(arr: ArrayProp): ArrayOutput {
-  let newArr = [] as ArrayOutput;
+  const newArr = [] as ArrayOutput;
 
   if (Object.prototype.toString.call(arr) === '[object Array]') {
     const len = arr.length;
